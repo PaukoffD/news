c12ec65972d6db513f1bbfa4d5f59d7e3ac2ad85
xuliaP <paukov2967@gmail.com>
2016-03-09 23:48:42 +0300

    0903_1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,52 +13,74 @@ class PagesController < ApplicationController
    i=0
    j=0
    feed = Feedjira::Feed.fetch_and_parse url
-  
+  plast=Newslast.find_by(source_id: s.id)
  
   feed.entries.each do |entry|
   if j<1
-    @p=Page.new
-    plast=Newslast.find_by(source_id: s.id)
+    
+   
    @p=Page.new
    @p.title=entry.title
     if !plast.blank?
         if plast.title==@p.title
 	  j=1
 	end  
-     @p.ref=entry.url
-     @p.time=entry.published
-     @p.source_id=s.id
-	 s2=entry.categories[0]
+        @p.ref=entry.url
+        @p.time=entry.published
+        @p.source_id=s.id
+	s2=entry.categories[0]
 	 
 	 cat1=Category.find_by(name: s2)
 	  if cat1.blank?
 	    c=Category.new
 		c.name=entry.categories[0]
+                if c.name==nil
+                   c.name=="Без категории"
+                end
 		c.save
 		cat1=Category.last
 	  end	
-     @p.category_id=cat1.id
-     @p.summary=entry.summary
-     @p.save
+       @p.category_id=cat1.id
+       @p.summary=entry.summary
+       @p.save
     else
-     if i==feed.entries.length-1
-	 @p1=Newslast.new
-	  @p1.title=entry.title
-	  @p1.time=entry.published
-          @p1.source_id=s.id
-	 # @p1.save
-     else
-         @p1=Newslast.new
-	  @p1.title=entry.title
-	  @p1.time=entry.published
-          @p1.source_id=s.id
-	  @p1.save
-     end 
-	 i=i+1
+      @p.ref=entry.url
+        @p.time=entry.published
+        @p.source_id=s.id
+	s2=entry.categories[0]
+	 
+	 cat1=Category.find_by(name: s2)
+	  if cat1.blank?
+	    c=Category.new
+		c.name=entry.categories[0]
+                if c.name==nil
+                   c.name=="Без категории"
+                end
+		c.save
+		cat1=Category.last
+	  end	
+       @p.category_id=cat1.id
+       @p.summary=entry.summary
+       @p.save
+    end
+    
+    
     end 
+    
   end
+      Page.all.order('time DESC')
+     @page1=Page.find_by(source_id: s)
+     @p1=Newslast.new
+     @p1.title=@page1.title
+     @p1.time=@page1.created_at
+     @p1.source_id=s.id
+     @p1.ref=@page1.ref
+      
+     @p1.save
+     @page2=Newslast.first
+     @page2.delete 
  end
-end
+
   
  
   
