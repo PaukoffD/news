e63cc746fb4310becc6c7d5cdf616ccf39764189
xuliaP <paukov2967@gmail.com>
2016-03-13 22:19:45 +0300

    1303

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -133,10 +133,11 @@ class PagesController < ApplicationController
    ActsAsTaggableOn.delimiter = ([' ', ','])
    @pages = Page.all
     @pages.each do |pt|
-    pt.tag_list.add(pt.title, parse: true)
-   
-	pt.save
-
+     if pt.tag_list.blank?
+      pt.tag_list.add(pt.title, parse: true)
+      pt.save
+     end
+     puts pt.tag_list
     end   
   end 
   
