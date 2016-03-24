39b83b48cf27b7a8032fc27d3b2c5810cbe507c3
xuliaP <paukov2967@gmail.com>
2016-03-24 14:20:10 +0300

    '2403_3'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,108 +7,72 @@ class PagesController < ApplicationController
   # GET /pages
   # GET /pages.json
   def load
-  source=Source.all
+   source=Source.all
 
-  source.each do |s|
-   url=s.ref
-   i=0
-   j=0
-   feed = Feedjira::Feed.fetch_and_parse url
-  plast=Newslast.find_by(source_id: s.id)
+   source.each do |s|
+    url=s.ref
+    i=0
+    j=0
+    feed = Feedjira::Feed.fetch_and_parse url
+    plast=Newslast.find_by(source_id: s.id)
  
-  feed.entries.each do |entry|
-  if j<1
+    feed.entries.each do |entry|
+     if j<1
     
    
-   @p=Page.new
-   @p.title=entry.title
-    if !plast.blank?
+     @p=Page.new
+     @p.title=entry.title
+     if !plast.blank?
         if plast.title==@p.title
-   j=1
- end  
-        @p.ref=entry.url
-        @p.time=entry.published.to_datetime
-        @p.source_id=s.id
- s2=entry.categories[0]
+           j=1
+        end  
+       @p.ref=entry.url
+       @p.time=entry.published.to_datetime
+       @p.source_id=s.id
+       s2=entry.categories[0]
   
-  cat1=Category.find_by(name: s2)
-   if cat1.blank?
-     c=Category.new
-  c.name=entry.categories[0]
+       cat1=Category.find_by(name: s2)
+      if cat1.blank?
+       c=Category.new
+       c.name=entry.categories[0]
                 if c.name==nil
                    c.name=="Без категории"
                 end
-  c.save
-  cat1=Category.last
-   end 
+       c.save
+       cat1=Category.last
+      end 
        @p.category_id=cat1.id
        @p.summary=entry.summary
        @p.save
-    else
-      @p.ref=entry.url
-        @p.time=entry.published.to_datetime
-        @p.source_id=s.id
- s2=entry.categories[0]
+      else
+       @p.ref=entry.url
+       @p.time=entry.published.to_datetime
+       @p.source_id=s.id
+       s2=entry.categories[0]
   
-  cat1=Category.find_by(name: s2)
-   if cat1.blank?
-     c=Category.new
-  c.name=entry.categories[0]
+       cat1=Category.find_by(name: s2)
+        if cat1.blank?
+          c=Category.new
+          c.name=entry.categories[0]
                 if c.name==nil
                    c.name=="Без категории"
                 end
-  c.save
-  cat1=Category.last
-   end 
-       @p.category_id=cat1.id
-       if entry.summary.blank?
+          c.save
+          cat1=Category.last
+        end 
+        @p.category_id=cat1.id
+         if entry.summary.blank?
         
-         entry.summary=" "
-       else  
-        @p.summary=entry.summary
-       end 
-       @p.save
-    end
-    
-    
+          entry.summary=" "
+         else  
+          @p.summary=entry.summary
+         end 
+        @p.save
+      end
     end 
-    
-#  end
-#fixme не работает эта байда    
-# Page.all.reorder('created_at DESC')
-#     @page1=Page.all.where(source_id: s)
-#  @p1=Newslast.new
- 
-#  @p1.title=@page1.last.title
-#     @p1.time=@page1.last.created_at
-#     @p1.source_id=s.id
-#     @p1.ref=@page1.last.ref
-#     @p1.save
-#  tmp=Newslast.all.where(source_id: s.id)
-#  if tmp.count >1
-#    p3=Newslast.first
-#    p3.delete
-#  end  
- end
-
-  
- 
-  
- # page3 = Nokogiri::HTML(open("http://www.vedomosti.ru/newsline/top"))
- #  page3.xpath("//article[@class='b-news-item']").each do |link|
-   
-  #  @p=Page.new
-# @p.title=link.at_css('a').text
-# @p.ref="http://www.vedomosti.ru"+link.at_css('a')['href']
-# @p.time=link.at_css('.b-news-item__time')['pubdate'].to_datetime
- 
-# @p.save
-
-  
-  
- 
- end 
-  
+  end
+end 
+end
   
   def analyze
    @pages=Page.all
