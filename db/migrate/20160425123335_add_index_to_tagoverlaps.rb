1edef347ec7f794ae1e390f2eacc6a07175b65a5
xuliaP <paukov2967@gmail.com>
2016-04-25 16:33:34 +0300

    'tagoverlap1'

--- db/migrate/20160425123335_add_index_to_tagoverlaps.rb
+++ db/migrate/20160425123335_add_index_to_tagoverlaps.rb
@@ -2,6 +2,6 @@ class AddIndexToTagoverlaps < ActiveRecord::Migration
   def change
     
     add_index :tagoverlaps, :name, unique: true
-	add_index :tagoverlaps, :nametarget, unique: true
+	add_index :tagoverlaps, :nametarget
   end
 end
