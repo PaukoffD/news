13b0550e8e5a3cadd62d57261c9725145dacce8b
xuliaP <paukov2967@gmail.com>
2016-05-14 23:38:31 +0300

    redis fix

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -18,6 +18,7 @@ module PagesHelper
 	require 'will_paginate/array' 
 def fetch_news
   @pages =  $redis.get("pages")
+  
   if @pages.nil?
      @pages = Page.all.order('time DESC').page(params[:page]).to_json
     $redis.set("pages", @pages)
@@ -25,5 +26,15 @@ def fetch_news
     $redis.expire("pages",15.minute.to_i)
   end
   @pages = JSON.load @pages
+
+  #@sources =  $redis.get("sourses")
+  #if @sources.nil?
+ #    @sources = Source.all.to_json
+  #  $redis.set("sources", @sources)
+    # Expire the cache, reorder('time DESC').page(params[:page]).very 5 hours
+ #   $redis.expire("sources",5.hours.to_i)
+ # end
+ 
+ # @sources = JSON.load @sources
 end
 end
