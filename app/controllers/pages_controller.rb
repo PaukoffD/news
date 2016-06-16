c5fc5b6d54ac1b5ad0942bb5ee7ae31f68db2555
xuliaP <paukov2967@gmail.com>
2016-06-16 15:18:40 +0300

    info tasks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -92,12 +92,13 @@ class PagesController < ApplicationController
 
   def rss
     @source = Source.all
-    lo
+    #lo
   end
 
   
 
   def info
+   @info=Info.first
    @pages = Page.all.count
    @tags = ActsAsTaggableOn::Tag.all.count
    @taggings = ActsAsTaggableOn::Tagging.all.count
@@ -141,6 +142,11 @@ def infoday1
    @source.each do |a|
     info=Info.find_by_source_id(a.id) || Info.new
     count=Page.where(source_id: a.id).count
+    @pg=Page.where(source_id: a.id).pluck(:id)
+    tagging_count=ActsAsTaggableOn::Tagging.where(taggable_id: @pg).count 
+    @tg=ActsAsTaggableOn::Tagging.select('distinct tag_id').where(taggable_id: @pg).count
+    info.tag_count=@tg
+    info.tagging=tagging_count 
     info.source_id=a.id
     info.page_count=count
     info.save
@@ -151,6 +157,12 @@ def infoday1
     info=Info.new
     count=Page.where(source_id: a.id, time: ((Date.today-i).to_time.beginning_of_day..(Date.today-i).to_time.end_of_day)).count
     @tags = ActsAsTaggableOn::Tag.all.count
+    @pg=Page.where(source_id: a.id, time: ((Date.today-i).to_time.beginning_of_day..(Date.today-i).to_time.end_of_day)).pluck(:id)
+    tagging_count=ActsAsTaggableOn::Tagging.where(taggable_id: @pg).count 
+    info.tagging=tagging_count
+    @tg=ActsAsTaggableOn::Tagging.select('distinct tag_id').where(taggable_id: @pg).count
+    #tg1=@tg.distinct.count
+    info.tag_count=@tg
     puts count
     info.source_id=a.id
     info.page_count=count
--- lib/tasks/infoday.rake
+++ lib/tasks/infoday.rake
@@ -1,14 +1,15 @@
+#Создает первую запись и считает ...раз в день
 task infoday: :environment do
-	  puts "beginning"
+	  
    @pages = Page.all.count
    @tags = ActsAsTaggableOn::Tag.all.count
    @taggings = ActsAsTaggableOn::Tagging.all.count
    @source = Source.all.count
    info=Info.first || Info.new
-    puts info.page_count=@pages
+   info.page_count=@pages
 
-    puts info.tag_count=@tags
-    puts info.tagging=@taggings 
-    puts info.size=@source
-    info.save
+   info.tag_count=@tags
+   info.tagging=@taggings 
+   info.size=@source
+   info.save
 end
